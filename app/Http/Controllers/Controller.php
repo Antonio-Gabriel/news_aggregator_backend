b081a37c3e0391fbc8acecbda51caa9b00876cef
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-11 07:27:45 +0100

    docs: adding swagger documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\PathItem(path="/api")
+ * @OA\Info( version="1.0.0", title="News Aggregator",
+ *    description="An application to aggregate news based to external news website"
+ * )
+ * 
+ * @OA\Get(
+ *     path="/api",
+ *     tags={"Welcome"},
+ *     description="Welcome page",
+ *     @OA\Response(response="200", description="Welcome page")
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
