e08a99b7d11f8088a40a7e0781695a371426c0d2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-12 12:02:47 +0100

    chore: change user povider dir

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\UserServiceProvider::class
+        App\Providers\User\UsersServiceProvider::class
     ])->toArray(),
 
     /*
