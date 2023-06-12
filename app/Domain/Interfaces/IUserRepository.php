45d31d03a891541c2b4352756f122662c60550ec
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-12 12:01:43 +0100

    chore: add new methods

--- app/Domain/Interfaces/IUserRepository.php
+++ app/Domain/Interfaces/IUserRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Domain\Interfaces;
 
-use Ramsey\Uuid\Guid\Guid;
 use Illuminate\Support\Collection;
 use App\Domain\Commands\UserCommand;
 use Illuminate\Database\Eloquent\Model;
@@ -15,5 +14,9 @@ interface IUserRepository
 
     public function create(UserCommand $entity): ?Model;
 
-    public function getById(Guid $userId): ?Model;
+    public function update(UserCommand $entity, int $id): bool;
+
+    public function getByEmail(string $email): ?Model;
+
+    public function getById(int $id): ?Model;
 }
