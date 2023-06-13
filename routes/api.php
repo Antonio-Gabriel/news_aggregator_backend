68bb6070eec53a1a7f048ffc5fc61deb00fe05de
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-13 22:39:16 +0100

    chore: add new routes and auth middleware

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::group(['prefix' => 'v1'], function () {
-    Route::apiResource('users', UserController::class);
-});
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('/', fn () => ["msg" => "welcome to our system"]);
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'v1'
+], function () {
+    Route::apiResource('users', UserController::class);
+});
+
+// Route::middleware('auth:api')->get('/user', [AuthController::class, 'getMe']);
+
+Route::post('/login', [AuthController::class, 'signIn']);
+Route::post('/logout', [AuthController::class, 'signOut']);
