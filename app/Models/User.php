30aec529566202f251e1a8e70b94843635d3d9d5
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-13 22:38:55 +0100

    chore: implement jwt interface

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -38,7 +39,17 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [        
+    protected $casts = [
         'password' => 'hashed',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
