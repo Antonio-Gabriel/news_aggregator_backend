a1507762e4b45c88b1230685ca9e829e2f814595
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-13 17:40:29 +0100

    feat: create update user usecase

--- app/Domain/System/UseCases/UpdateUserUsecase.php
+++ app/Domain/System/UseCases/UpdateUserUsecase.php
@@ -5,7 +5,7 @@ namespace App\Domain\System\UseCases;
 use App\Domain\Commands\UserCommand;
 use App\Domain\Interfaces\IUserRepository;
 use App\Exceptions\User\UserDoesntExists;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class UpdateUserUsecase
@@ -17,11 +17,24 @@ class UpdateUserUsecase
 
     public function execute(UserCommand $user, int $id)
     {
-        if (!$this->repository->exists($user->email)) {
-            Log::info("User doesn't exists", [$user->email]);
+        if (!$this->repository->getById($id)) {
+            Log::info("User doesn't exists", [$id]);
             throw new UserDoesntExists("User doesn't exists");
         }
 
+        $user->password = $this->isUpdatedPassword($user, $id);
+
         return $this->repository->update($user, $id);
+    }
+
+    private function isUpdatedPassword(UserCommand $user, int $id)
+    {
+        $userData = $this->repository->getById($id);
+
+        if (!Hash::check($user->password, $userData->password)) {
+            return bcrypt($user->password);
+        }
+
+        return $userData->password;
     }
 }
