9dac78852bc8620a3987731bd58cf45854ffdbc3
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-13 22:40:37 +0100

    chore: configure providers and alias

--- config/app.php
+++ config/app.php
@@ -168,7 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\User\UsersServiceProvider::class
+        App\Providers\User\UsersServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class
+
     ])->toArray(),
 
     /*
@@ -184,6 +186,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'JWTAuth' => \Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => \Tymon\JWTAuth\Facades\JWTFactory::class
     ])->toArray(),
 
 ];
