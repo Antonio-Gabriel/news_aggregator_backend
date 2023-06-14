9a03536677a3a99b5ec6ece43dfc2df46779ef6c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-14 10:15:10 +0100

    chore: add user auth provider

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\User\UsersServiceProvider::class,
+        App\Providers\User\UserAuthProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class
 
     ])->toArray(),
