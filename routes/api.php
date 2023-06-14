f1daaed8452b10a94793dc9f3a35ad9403f9cfa5
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-14 08:14:25 +0100

    chore: remove middleware unused

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => ["msg" => "welcome to our system"]);
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'v1'
-], function () {
+Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('users', UserController::class);
 });
 
