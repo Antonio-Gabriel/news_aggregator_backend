e3bb6879ec419fb990dda758a6c5fae4efda3a99
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-14 09:47:29 +0100

    chore: create auth documentation schema

--- app/Http/Requests/AuthUserRequest.php
+++ app/Http/Requests/AuthUserRequest.php
@@ -3,7 +3,38 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Request;
+
+/**
+ * Class AuthUserRequest
+ * @package App\Http\Requests
+ * @OA\Schema(
+ *     schema="AuthUserRequest",
+ *     type="object",
+ *     title="Auth user request",
+ *     properties={
+ *         @OA\Property(property="user", type="object", properties={
+ *              @OA\Property(property="id", type="int"),
+ *              @OA\Property(property="name", type="string"),
+ *              @OA\Property(property="email", type="string"),
+ *              @OA\Property(property="created_at", type="string"),
+ *              @OA\Property(property="updated_at", type="string") 
+ *         }),
+ *         @OA\Property(property="authentication", type="object", properties={
+ *              @OA\Property(property="token", type="string"),
+ *              @OA\Property(property="type", type="string"),
+ *              @OA\Property(property="expires_in", type="int"),
+ *         })
+ *     }
+ * )
+ * @OA\Schema(
+ *     schema="AuthUserRequestValidationError",
+ *     type="object",
+ *     title="AuthUserRequestValidationError",
+ *     properties={
+ *         @OA\Property(property="error", type="string")
+ *     }
+ * )
+ */
 
 class AuthUserRequest extends FormRequest
 {
