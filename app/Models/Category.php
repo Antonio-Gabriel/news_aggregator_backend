d29950c7c9a92a63824901c09c29567be7b903ff
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-14 21:48:29 +0100

    chore: add article relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
 {
@@ -17,4 +19,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function article(): HasOne
+    {
+        return $this->hasOne(Article::class);
+    }
 }
