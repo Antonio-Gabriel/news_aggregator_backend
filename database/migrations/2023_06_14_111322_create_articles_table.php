a5c69d6f1da0a8049e3bf6f0410895b2da7ac870
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-14 21:47:28 +0100

    chore: article migration

---
+++ database/migrations/2023_06_14_111322_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('source')->nullable();
+            $table->string('author')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->string('url')->nullable();
+            $table->string('url_image')->nullable();
+            $table->dateTime('published_at');
+            $table->timestamps();
+            $table->foreign('category_id', 'categoryArticle')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
