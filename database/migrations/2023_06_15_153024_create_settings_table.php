86613a8866bca95c050c564f6f46b0d87788cb0e
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-15 18:49:51 +0100

    chore: create settings migration

---
+++ database/migrations/2023_06_15_153024_create_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id', 'UserSettings')
+                ->references('id')
+                ->on('users')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
