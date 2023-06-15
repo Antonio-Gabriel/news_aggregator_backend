4139fc60b3ba7a98bc643c5794a5a8463a563fc7
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-16 00:20:06 +0100

    chore: add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Antonio Gabriel',
+            'email' => 'antoniogabriel@gmail.com',
+            'password' => bcrypt('antoniogabriel'),
+        ]);
+
+        $categoriesSeed = [
+            ['name' => 'Digital Transformation'],
+            ['name' => 'Innovation'],
+            ['name' => 'Work & life'],
+            ['name' => 'News'],
+            ['name' => 'Diversity & Inclusion'],
+            ['name' => 'Sustainability']
+        ];
+
+        foreach ($categoriesSeed as $category) {
+            Category::create($category);
+        }
     }
 }
