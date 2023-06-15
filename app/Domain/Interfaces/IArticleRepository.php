91ae7ea538640cff6f29ae7a549a421d72338ad5
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-15 16:25:24 +0100

    chore: change interface method to getquery

--- app/Domain/Interfaces/IArticleRepository.php
+++ app/Domain/Interfaces/IArticleRepository.php
@@ -11,7 +11,7 @@ interface IArticleRepository
     public function get(): Collection;
     public function getById(int $id): ?Model;
     public function exists(string $title, int $categoryId): bool;
-    public function getByFilters(array $filters): Collection;
+    public function getQuery();
     public function create(ArticleCommand $entity): ?Model;
     public function update(ArticleCommand $entity, int $id): bool;
 }
