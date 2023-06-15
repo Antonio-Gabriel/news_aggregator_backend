88b37bc563a48241684d577a62d3f02b70e38548
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-15 18:52:26 +0100

    chore: add relationship with settings

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -51,5 +52,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function settings(): HasOne
+    {
+        return $this->hasOne(Settings::class);
     }
 }
